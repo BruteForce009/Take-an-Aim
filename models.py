8c08ce4b22239a62eb9d13e4cbade27c346821de
BruteForce009 <1517satyamtomar@gmail.com>
2022-06-03 20:00:29 +0530

    Improved Interface. Added new style-sheets. Updated About section.

--- models.py
+++ models.py
@@ -3,7 +3,7 @@ from app import db
 
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), nullable=False)
+    username = db.Column(db.String(30), nullable=False)
     date = db.Column(db.Date, nullable=False)
 
     def __repr__(self):
